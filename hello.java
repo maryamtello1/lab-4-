f9c8f8c0279d7bdb01a19c9618d1550b7b446da9
maryamtello1 <maryamt@umich.edu>
2023-02-09 15:25:59 -0500

    main

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello {
 public static void main(String[] args) {
 for(int i=1;i<=10;i++) {
 System.out.println("Hello World How are you doing?..."+i);
+System.out.println("I am fine"+i);
 }
 }
 }
